fb61a50ed358c5ede912468cf33d60ba418dcec2
ainamysara <84973388+ainamysara@users.noreply.github.com>
2022-07-04 17:23:06 +0800

    initial commit

--- KOINEX/resources/views/index.blade.php
+++ KOINEX/resources/views/index.blade.php
@@ -194,6 +194,11 @@
         <hr class="solid">  
     </header>
    
+    <div style="height:62px; background-color: #1D2330; overflow:hidden; box-sizing: border-box; border: 1px solid #282E3B; border-radius: 4px; text-align: right; line-height:14px; block-size:62px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #262B38;padding:1px;padding: 0px; margin: 0px; width: 100%;">
+        <div style="height:40px; padding:0px; margin:0px; width: 100%;">
+            <iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1521&invert_hover=no" width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe>
+        </div>
+    </div>
     <footer class="footer nt-auto py-3 bg-dark">
         <div class="container text-center">
            <span class="text-muted">@Cryptocurrency(2022)</span>
