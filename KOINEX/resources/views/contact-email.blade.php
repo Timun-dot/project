ffada098a1718eec94b2dc75b2cd2d3cf0d9f790
ainamysara <84973388+ainamysara@users.noreply.github.com>
2022-07-14 10:43:46 +0800

    edit

--- KOINEX/resources/views/contact-email.blade.php
+++ KOINEX/resources/views/contact-email.blade.php
@@ -5,5 +5,5 @@ You received an email from : {{ $name }}
 </br><b>Email:</b> {{ $email }}
 </br><b>Phone Number:</b> {{ $phone_number }}
 </br><b>Subject:</b> {{ $subject }}
-<br><b>Message:</b> {{ $user_message }}
-Thank You
+</br><b>Message:</b> {{ $user_message }}
+</br>Thank You
--- KOINEX/resources/views/contact-us.blade.php
+++ KOINEX/resources/views/contact-us.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Laravel 5.8 CRUD Example Tutorial</title>
+        <title>Crypto-Dashboard</title>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
